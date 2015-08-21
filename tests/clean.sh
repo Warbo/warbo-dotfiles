ed4c354b87213906539925df09b4737bc2975090
Chris Warburton <chriswarbo@gmail.com>
2015-08-21 17:31:37 +0100

    New tests

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,28 +1,2 @@
 #!/usr/bin/env bash
-
-function exists {
-    for S in scripts/*
-    do
-        NAME=$(basename "$S")
-        if [[ "x$NAME" = "x$1" ]]
-        then
-            return 0
-        fi
-    done
-    return 1
-}
-
-# Remove stderr/stdout files which don't correspond to a test
-for EXT in stderr stdout
-do
-    for F in results/*."$EXT"
-    do
-        NAME=$(basename "$F" ".$EXT")
-        if exists "$NAME"
-        then
-            true
-        else
-            rm "$F"
-        fi
-    done
-done
+./run clean
