29e3c351a2534fe9a02a564ab162155c228fe367
Chris Warburton <chriswarbo@gmail.com>
2015-10-14 16:54:00 +0100

    Changes

--- tests/default.nix
+++ tests/default.nix
@@ -4,32 +4,12 @@ stdenv.mkDerivation {
   name = "tests";
   src  = ./.;
   buildInputs = [
-    ArbitraryHaskell
-    coalp
-    get_iplayer
-    git2html
+    darcs
     gnumake
-    haskellPackages.random
     haskellPackages.hlint
     haskellPackages.packunused
     haskellPackages.ShellCheck
-    HS2AST
-    ml4hs
-    ml4pg
-    mlspec
-    panhandle
-    panpipe
-    quickspec
     subversionClient
-    te-unstable.ArbitraryHaskell
-    te-unstable.AstPlugin
-    te-unstable.HS2AST
-    te-unstable.ml4hs
-    te-unstable.mlspec
-    te-unstable.treefeatures
-    treefeatures
-    warbo-utilities
-    weka
     which
     xidel
   ];
