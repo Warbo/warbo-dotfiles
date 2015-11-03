e9311014e23d35abfb605c06c5da7060a59d527a
Chris Warburton <chriswarbo@gmail.com>
2015-11-03 10:33:09 +0000

    Remove unused JS

--- conkerorrc/block_js.js
+++
@@ -1,8 +0,0 @@
-require("content-policy.js");
-
-function block_js (content_type, content_location) {
-    return content_location.spec.match(/js\/bg\/)? content_policy_accept
-                                                 : content_policy_reject;
-}
-//content_policy_bytype_table.object = block_js;
-//add_hook("content_policy_hook", content_policy_bytype);
