9beacd7de5be37e92323a4d44391eb2945b39d4a
Chris Warburton <chriswarbo@gmail.com>
2016-04-22 13:49:36 +0100

    Disable MESA fix, unless things are too unstable

--- config/imm/imm.hs
+++ config/imm/imm.hs
@@ -11,41 +11,42 @@ myFeeds = zip (repeat config) myUris
 
 -- Uris are bare String and will be parsed inside imm
 myUris =  map snd [
-  ("Planet Haskell",
-   "http://planet.haskell.org/rss20.xml"),
-
-  ("XKCD",
-   "http://xkcd.com/rss.xml"),
-
-  ("XKCD What If",
-   "http://what-if.xkcd.com/feed.atom"),
-
-  ("Saturday Morning Breakfast Cereal",
-   "http://www.smbc-comics.com/rss.php"),
-
-  ("Perry Bible Fellowship",
-   "http://pbfcomics.com/feed/feed.xml"),
-
-  ("Flying Colours Maths",
-   "http://www.flyingcoloursmaths.co.uk/feed/"),
-
-  ("Intelligence: Born Smart, Born Equal, Born Different",
-   "http://www.bbc.co.uk/programmes/b042q944/episodes/downloads.rss"),
-
-  ("Hidden Histories of the Information Age",
-   "http://www.bbc.co.uk/programmes/b04mttrp/episodes/downloads.rss"),
 
   ("Computing Britain",
    "http://www.bbc.co.uk/programmes/b06bq6j1/episodes/downloads.rss"),
 
-  ("More or Less",
-   "http://www.bbc.co.uk/programmes/p02nrss1/episodes/downloads.rss"),
+  ("Conor McBride",
+   "https://pigworker.wordpress.com/feed/"),
 
-  ("The Math Factor Podcast",
-   "http://mathfactor.uark.edu/feed/"),
+  ("Flying Colours Maths",
+   "http://www.flyingcoloursmaths.co.uk/feed/"),
+
+  ("Hidden Histories of the Information Age",
+   "http://www.bbc.co.uk/programmes/b04mttrp/episodes/downloads.rss"),
+
+  ("Intelligence: Born Smart, Born Equal, Born Different",
+   "http://www.bbc.co.uk/programmes/b042q944/episodes/downloads.rss"),
+
+  ("Lambda the Ultimate",
+   "http://lambda-the-ultimate.org/rss.xml")
 
   ("Linear Digressions",
    "http://feeds.feedburner.com/udacity-linear-digressions"),
+
+  ("MIRI",
+   "http://feeds.feedburner.com/miriblog"),
+
+  ("More or Less",
+   "http://www.bbc.co.uk/programmes/p02nrss1/episodes/downloads.rss"),
+
+  ("Perry Bible Fellowship",
+   "http://pbfcomics.com/feed/feed.xml"),
+
+  ("Planet Haskell",
+   "http://planet.haskell.org/rss20.xml"),
+
+  ("Saturday Morning Breakfast Cereal",
+   "http://www.smbc-comics.com/rss.php"),
 
   ("Talking Machines",
    "http://www.thetalkingmachines.com/blog/?format=RSS"),
@@ -56,17 +57,17 @@ myUris =  map snd [
   ("The Life Scientific",
    "http://www.bbc.co.uk/programmes/b015sqc7/episodes/downloads.rss"),
 
+  ("The Math Factor Podcast",
+   "http://mathfactor.uark.edu/feed/"),
+
   ("The Type Theory Podcast",
    "http://typetheorypodcast.com/feed/podcast"),
 
-  ("MIRI",
-   "http://feeds.feedburner.com/miriblog"),
+  ("XKCD",
+   "http://xkcd.com/rss.xml"),
 
-  ("Conor McBride",
-   "https://pigworker.wordpress.com/feed/"),
-
-  ("Lambda the Ultimate",
-   "http://lambda-the-ultimate.org/rss.xml")
+  ("XKCD What If",
+   "http://what-if.xkcd.com/feed.atom")
   ]
 
 config :: Config -> Config
