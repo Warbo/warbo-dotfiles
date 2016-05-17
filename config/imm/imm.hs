c69ac299b997018f83e2383a0abaea59df97e17a
Chris Warburton <chriswarbo@gmail.com>
2016-05-17 15:10:41 +0100

    Read RSS files from disk

--- config/imm/imm.hs
+++ config/imm/imm.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Control.Lens
 import Imm
+import System.Directory
+import System.Environment
+import System.IO.Unsafe
 
 main = imm myFeeds
 
@@ -10,86 +13,17 @@ myFeeds :: [ConfigFeed]
 myFeeds = zip (repeat config) myUris
 
 -- Uris are bare String and will be parsed inside imm
-myUris =  map snd [
-
-  ("Computing Britain",
-   "http://www.bbc.co.uk/programmes/b06bq6j1/episodes/downloads.rss"),
-
-  ("Conor McBride",
-   "https://pigworker.wordpress.com/feed/"),
-
-  ("Flying Colours Maths",
-   "http://www.flyingcoloursmaths.co.uk/feed/"),
-
-  ("Hidden Histories of the Information Age",
-   "http://www.bbc.co.uk/programmes/b04mttrp/episodes/downloads.rss"),
-
-  ("Intelligence: Born Smart, Born Equal, Born Different",
-   "http://www.bbc.co.uk/programmes/b042q944/episodes/downloads.rss"),
-
-  ("iPlayer Comedy",
-   "http://localhost:8888/iplayer_feeds/comedy.rss"),
-
-  ("iPlayer Films",
-   "http://localhost:8888/iplayer_feeds/films.rss"),
-
-  ("iPlayer Science/Nature",
-   "http://localhost:8888/iplayer_feeds/scinat.rss"),
-
-  ("Kurzweil AI",
-   "http://localhost:8888/rss/kurzweilai.rss"),
-
-  ("Lambda the Ultimate",
-   "http://lambda-the-ultimate.org/rss.xml"),
-
-  ("Linear Digressions",
-   "http://feeds.feedburner.com/udacity-linear-digressions"),
-
-  ("MIRI",
-   "http://feeds.feedburner.com/miriblog"),
-
-  ("More or Less",
-   "http://www.bbc.co.uk/programmes/p02nrss1/episodes/downloads.rss"),
-
-  ("Perry Bible Fellowship",
-   "http://pbfcomics.com/feed/feed.xml"),
-
-  ("Planet Haskell",
-   "http://planet.haskell.org/rss20.xml"),
-
-  ("Saturday Morning Breakfast Cereal",
-   "http://www.smbc-comics.com/rss.php"),
-
-  ("Talking Machines",
-   "http://www.thetalkingmachines.com/blog/?format=RSS"),
-
-  ("TED Talks",
-   "https://www.ted.com/talks/rss"),
-
-  ("The Life Scientific",
-   "http://www.bbc.co.uk/programmes/b015sqc7/episodes/downloads.rss"),
-
-  ("The Math Factor Podcast",
-   "http://mathfactor.uark.edu/feed/"),
-
-  ("The Type Theory Podcast",
-   "http://typetheorypodcast.com/feed/podcast"),
-
-  ("XKCD",
-   "http://xkcd.com/rss.xml"),
-
-  ("XKCD What If",
-   "http://what-if.xkcd.com/feed.atom"),
-
-  ("YouTube - Computerphile",
-   "http://localhost:8888/rss/computerphile.rss"),
-
-  ("YouTube - Numberphile",
-   "http://localhost:8888/rss/numberphile.rss"),
-
-  ("YouTube - Matt Parker",
-   "http://localhost:8888/rss/standupmaths.rss")
-  ]
+myUris =  map ("http://localhost:8888/" ++) filesInCache
 
 config :: Config -> Config
 config = set maildir "/home/chris/.imm-feeds/feeds"
+
+isRss = (".rss" `isSuffixOf`)
+
+filesInCache :: [FilePath]
+filesInCache = unsafePerformIO $ do
+  rss     <- getDirectoryContents "/home/chris/.cache/rss"
+  iplayer <- getDirectoryContents "/home/chris/.cache/iplayer_feeds"
+  let rss'     = map           ("rss/" ++) (filter isRss rss)
+      iplayer' = map ("iplayer_feeds/" ++) (filter isRss iplayer)
+  return (rss' ++ iplayer')
