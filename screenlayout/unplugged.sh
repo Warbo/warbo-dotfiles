685b6a2c5b777876d59e990a3aadba6cfb0af54f
Chris Warburton <chriswarbo@gmail.com>
2016-12-08 11:40:21 +0000

    Fix unplugged layout

--- screenlayout/unplugged.sh
+++ screenlayout/unplugged.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1200x1152 --rotate normal --output DP1 --off --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output LVDS1 --mode 1024x768 --pos 0x0 --rotate normal --output TV1 --off --output VGA1 --off
