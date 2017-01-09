8398d1e18581f99309a33d3b1fdc9842b710e33d
Chris Warburton <chriswarbo@gmail.com>
2017-01-09 17:33:43 +0000

    Use emacs from Conkeror

--- conkerorrc/main.js
+++ conkerorrc/main.js
@@ -3,12 +3,19 @@ url_remoting_fn = load_url_in_new_buffer;
 
 // Load some useful modes
 require('gmail');
-require('github');
+//require('github');
 require('duckduckgo');
 require('xkcd');
 xkcd_add_title = true;
 
 define_webjump('imdb', 'http://www.imdb.com/find?q=%s');
+
+// Stop sites like github from disabling our browser keys
+require("key-kill");
+key_kill_mode.test.push(build_url_regexp($domain = "github"));
+
+view_source_use_external_editor = true;
+editor_shell_command = "emacsclient -c";
 
 //io_service.manageOfflineStatus = false;
 //io_service.offline = false;
