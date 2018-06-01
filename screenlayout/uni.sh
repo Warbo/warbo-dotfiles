6762a74b9b34b70e8f2574396b3614cc56ae08ad
Chris Warburton <chriswarbo@gmail.com>
2018-06-01 15:20:08 +0100

    Make Uni monitor "primary", so xmobar and xmonad work together
    Without this, Xmonad seems to leave a space for xmobar on the external monitor,
    but actually displays it on the internal monitor (behind any other windows).
    
    Here we add the '--primary' option for our plug/unplug scripts, which causes
    xmobar to appear in the space that xmonad gives it on the external monitor. When
    unplugged, the space appears on the internal monitor and xmobar appears as
    expected.

--- screenlayout/uni.sh
+++ screenlayout/uni.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output LVDS1 --mode 1024x768 --pos 1080x1152 --rotate normal --output TV1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output VIRTUAL1 --off --output LVDS1 --mode 1024x768 --pos 1080x1152 --rotate normal --output TV1 --off --output VGA1 --primary --mode 1920x1080 --pos 0x0 --rotate left
