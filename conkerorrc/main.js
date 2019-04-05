73bccc166081d3591312996a5dd248d01021ccc5
Chris Warburton <chriswarbo@gmail.com>
2019-04-05 10:13:20 +0100

    Switch homepage to laminar

--- conkerorrc/main.js
+++ conkerorrc/main.js
@@ -1,7 +1,7 @@
 // Use an existing window if there is one
 url_remoting_fn = load_url_in_new_buffer;
 
-homepage = "http://localhost:4000";
+homepage = "http://localhost:8008";
 
 // Load some useful modes
 require('gmail');
