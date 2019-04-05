797bfff8ea0994beb6de10153e21ffc295280095
Chris Warburton <chriswarbo@gmail.com>
2019-04-05 10:13:06 +0100

    Screen layouts

---
+++ screenlayout/projector.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output LVDS1 --primary --mode 1024x768 --pos 0x600 --rotate normal --output TV1 --off --output VGA1 --mode 800x600 --pos 0x0 --rotate normal
--- screenlayout/robs.sh
+++ screenlayout/robs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output LVDS1 --primary --mode 1024x768 --pos 0x0 --rotate normal --output TV1 --off --output VGA1 --mode 1024x768 --pos 1024x0 --rotate normal
+xrandr --output VIRTUAL1 --off --output LVDS1 --mode 1024x768 --pos 0x1024 --rotate normal --output TV1 --off --output VGA1 --primary --mode 1280x1024 --pos 712x0 --rotate normal
