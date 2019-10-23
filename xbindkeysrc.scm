6242ee0e9e52047301ef31f932946bd5cb6c5463
Chris Warburton <chriswarbo@gmail.com>
2019-10-23 10:23:30 +0100

    Re-add volume keybindings

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -7,3 +7,22 @@
 
 ;; Command hotkeys
 (xbindkey "Menu" "inv")
+
+;; Audio buttons
+(xbindkey "XF86AudioRaiseVolume" "amixer -c 0 sset Master 1+")
+(xbindkey "XF86AudioLowerVolume" "amixer -c 0 sset Master 1-")
+(xbindkey-function
+ "XF86AudioMute"
+ (let ((muted #f)
+       (cmd   (lambda (chan prefix)
+                (run-command (string-concatenate) '("amixer sset "
+                                                    chan
+                                                    " "
+                                                    prefix
+                                                    "mute")))))
+   (lambda ()
+     (cmd "Master" (if muted "un" ""))
+     (set! muted (not muted))
+
+     ;; amixer always mutes Headphone too, so unmute it
+     (cmd "Headphone" "un"))))
