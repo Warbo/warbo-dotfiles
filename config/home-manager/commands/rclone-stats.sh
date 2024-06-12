dafd0f8b48ff10910b542341bdb00bf04263e03c
Chris Warburton <chriswarbo@gmail.com>
2024-06-12 22:22:46 +0100

    Remove home-manager (migrated to nix-config.git)

--- config/home-manager/commands/rclone-stats.sh
+++
@@ -1,7 +0,0 @@
-# Poll our SFTP rclone mount for its disk usage and queue size
-while true
-do
-    rclone rc --rc-addr :22222 vfs/stats |
-        jq '.diskCache + {"MBs": (.diskCache.bytesUsed / 1000000)}'
-    sleep 5
-done
